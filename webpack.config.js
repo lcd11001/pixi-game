decc751920bc8716a6f2185c7cd2c955f14d4e79
Luong Cong Dan <>
2018-03-09 16:34:11 +0700

    + clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -15,7 +16,10 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management'
-    })
+    }),
+    new CleanWebpackPlugin([
+      'dist'
+    ])
   ],
 
   // mode: 'development'

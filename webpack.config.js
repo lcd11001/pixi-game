5fe56d45ea5d062b6bfd7bb8711370e7076a9816
Luong Cong Dan <>
2018-03-09 16:29:54 +0700

    + html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
 
   // mode: 'development'
   mode: 'production',

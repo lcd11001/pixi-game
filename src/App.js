24f11bd7fcea632a366f93b4d8238e8342502575
Luong Cong Dan <lcd11001@gmail.com>
2018-11-17 16:51:04 +0700

    - test pixi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Stage, Sprite } from '@inlet/react-pixi'
 
 class App extends Component {
-  render() {
-    return (
-      <h1>
-        hello pixi react
-      </h1>
-    );
-  }
+    render() {
+        return (
+            <Stage width={window.innerWidth} height={window.innerHeight} options={{ backgroundColor: 0xff00ff }}>
+                <Sprite image="./assets/images/bunny.png" x={100} y={100} />
+            </Stage>
+        )
+    }
 }
 
 export default App;
